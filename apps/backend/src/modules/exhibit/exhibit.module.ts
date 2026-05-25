90ab78d7a5a6b0b36bc939f8a734650642ca912c
Vanjarapu Chandan Kumar <gangaputrachandan@gmail.com>
2026-05-25 05:49:36 +0530

    feat(exhibit): import SocketModule for create-time broadcasts

--- apps/backend/src/modules/exhibit/exhibit.module.ts
+++ apps/backend/src/modules/exhibit/exhibit.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ExhibitService } from './exhibit.service';
 import { ExhibitController } from './exhibit.controller';
 import { PrismaModule } from '../prisma/prisma.module';
+import { SocketModule } from '../socket/socket.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, SocketModule],
   controllers: [ExhibitController],
   providers: [ExhibitService],
   exports: [ExhibitService],
