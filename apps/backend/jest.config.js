0e86c4687cee8df8e90b55a7a7c4a7d374a23484
Vanjarapu Chandan Kumar <gangaputrachandan@gmail.com>
2026-05-25 05:50:07 +0530

    chore(backend): add Jest config with ts-jest preset

---
+++ apps/backend/jest.config.js
@@ -0,0 +1,15 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  rootDir: '.',
+  roots: ['<rootDir>/src'],
+  testRegex: '.*\\.spec\\.ts$',
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  collectCoverageFrom: ['src/**/*.ts', '!src/main.ts', '!**/*.module.ts'],
+  coverageDirectory: 'coverage',
+  testTimeout: 10_000,
+};
